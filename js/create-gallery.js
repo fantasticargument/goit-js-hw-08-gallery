94c462ad97a3134b14e3c641d48e20c11715d818
Petryk Roman <romic.argument@gmail.com>
2021-05-30 13:20:01 +0300

    Update create-gallery.js

--- js/create-gallery.js
+++ js/create-gallery.js
@@ -2,13 +2,18 @@ import gallery from './gallery-items.js';
 
 const ref = {
   galeryList: document.querySelector('.js-gallery'),
-  modalCase: document.querySelector('.lightbox')
+  modalCase: document.querySelector('.lightbox'),
+  closedModalbatton: document.querySelector('.lightbox__button'),
+  currentImage: document.querySelector('.lightbox__image'),
+  beckDrop: document.querySelector
 }
 
 const makeGalleryList = templateItemGallery(gallery);
 ref.galeryList.insertAdjacentHTML('beforeend', makeGalleryList)
 
 ref.galeryList.addEventListener('click', openModal)
+ref.closedModalbatton.addEventListener('click', closedModal)
+
 
 function templateItemGallery(gallery) {
   return gallery.map(({ preview, original, description }) => {
@@ -29,12 +34,18 @@ function templateItemGallery(gallery) {
   }).join(' ');
 };
 
-function openModal (event){
-  if (!event.target.classList.contains('gallery__item')) {
+function openModal(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
     return;
   }
-  
-  console.log(event.target);
-  // elent.target.classList.add('is-open')
+  ref.modalCase.classList.add('is-open');
+  ref.currentImage.src = event.target.dataset.source;
+  ref.currentImage.alt = event.target.alt;
+};
+
+function closedModal(event) {
+  ref.currentImage.src = null;
+  ref.modalCase.classList.remove('is-open');
 }
- 
